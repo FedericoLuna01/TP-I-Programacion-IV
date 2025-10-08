b983a709ac23ffd3c7450812df9cddec1f405e36
Luciano Tessa <tessaluciano3@gmail.com>
2025-10-08 17:45:43 -0300

    added get user dto

--- backend/src/Api/Controllers/UserController.cs
+++ backend/src/Api/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Models.Users;
 using Domain.Dtos.User;
 using Domain.Entities;
 using Domain.Interfaces;
@@ -38,11 +39,10 @@ namespace Api.Controllers
 
 
         [HttpGet]
-        public IActionResult GetAll()
+        public ActionResult<List<UserDto>> GetAll()
         {
             var users = _userRepo.GetAll();
-            // TODO: Pasar el dto para no pasar la password
-            return Ok(users);
+            return UserDto.Create(users);
         }
 
         [HttpDelete]
@@ -59,7 +59,7 @@ namespace Api.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public IActionResult GetUserById([FromRoute] int id)
+        public ActionResult<UserDto> GetUserById([FromRoute] int id)
         {
             var user = _userRepo.GetById(id);
 
@@ -68,7 +68,7 @@ namespace Api.Controllers
                 return NotFound("User not found");
             }
 
-            return Ok(user);
+            return UserDto.Create(user);
         }
 
         [HttpPut]
